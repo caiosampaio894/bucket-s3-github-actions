2576cb2d61db3f431edd937ffc74918215ea4c9b
Caio <caio_sampaio894@gmail.com>
2023-05-05 19:04:39 -0300

    trying github actions again

--- main.tf
+++ main.tf
@@ -6,12 +6,34 @@ provider "aws" {
 
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "bucket-s3-api-cep"
-  acl    = "public-read"
 
-   website {
-  index_document = "index.html"
-  error_document = "error.html"
 }
+
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "website_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website_bucket,
+    aws_s3_bucket_public_access_block.website_bucket,
+  ]
+
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "website_bucket_policy" {
@@ -33,6 +55,16 @@ resource "aws_s3_bucket_policy" "website_bucket_policy" {
     ]
   })
 }
+
+
+terraform {
+  backend "s3" {
+    bucket = "buckt-s3-terraform-actions"
+    key    = "api-cep/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 
 
 
